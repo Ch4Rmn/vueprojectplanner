b42c7d2750c40c7022f29c4861e1b5d2a8f633d4
Ch4Rmn <linhtut123451@gmail.com>
2024-01-03 04:05:39 +0630

    done project plan box with bs

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="checkDone"> 
+  <div class="m-2 text-dark fs-4 fw-bold p-2 text-start ">
+
+    <div class="d-flex justify-content-between align-items-center ">
+      <div class="div">
+        <div class="" style="cursor: pointer" v-on:click="open()">
+          {{ item.title }}
+          <div class="fs-5" v-if="showDetail">
+            {{ item.detail }}
+          </div>
+        </div>
+      </div>
+
+      <!-- icons div  -->
+      <div class=" " style="cursor: pointer">
+        <span class="material-icons ms-1"> edit </span>
+        <span class="material-icons ms-1"> delete_forever </span>
+        <span class="material-icons ms-1"> check_circle </span>
+      </div>
+    </div>
+
+  </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SingleProject",
+
+  props: ["item"],
+
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {
+    open() {
+      this.showDetail = !this.showDetail;
+    },
+  },
+
+  components: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+span:hover {
+  text-shadow: 3px 3px rgb(255, 255, 255);
+}
+.div:hover {
+  text-shadow: 1px 1px rgb(255, 255, 255);
+}
+.checkDone{
+  border-left: 7px solid red;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-danger fs-3 fw-bold text-decoration-underline">
+      Project Planner 
+    </h1>
+      <!-- v-for="(task, index) in filterTask" :key="index" -->
+      <div
+        class="bg-primary my-3 p-3 shadow-lg "
+        v-for="(item, index) in data"
+        :key="index"
+      >
+        <!-- item sent parent to child with props :item then catch with props:[]-->
+        <SingleProject :item="item"></SingleProject>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProject from "../components/SingleProject.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { SingleProject },
+  data() {
+    return {
+      data: "",
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        this.data = data;
+      })
+      .catch((err) => console.log(err.message()));
+  },
+};
 </script>
